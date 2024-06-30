3a79e44d9865bc5d23bf9b099f92bc11f60a65c3
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2024-06-29 23:28:51 -0400

    repository done

--- features/shopSlice.js
+++ features/shopSlice.js
@@ -1,23 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
-import products from "../assets/data/products.json"
-import categories from "../assets/data/categories.json"
-
 
 export const shopSlice = createSlice({
   name: 'shop',
   initialState: {
-    products,
-    categories,
     categorySeleted: '',
     productIdSelected: '',
-    productsFilteredByCategory: [],
   },
   reducers: {
     setCategorySelected: (state, action) => {
       state.categorySeleted = action.payload
-      state.productsFilteredByCategory = state.products.filter(
-        product => product.brand === action.payload
-      )
     },
     setProductIdSelected: (state, action) => {
       state.productIdSelected = action.payload
